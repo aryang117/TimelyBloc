ac64c9268225360a455a24d5d26aa890d9e60766
Trolled00Gamer <aryu100@gmail.com>
2021-09-07 17:13:29 +0530

    buttons are now textbase instead of iconbased

--- lib/Screens/actions.dart
+++ lib/Screens/actions.dart
@@ -17,45 +17,77 @@ class ActionsWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               if (timerState is TimerInitial) ...[
-                IconButton(
-                  iconSize: 65,
+                MaterialButton(
+                  color: Colors.redAccent,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      width: 60,
+                      child: Text('START')),
                   onPressed: () => context
                       .read<TimerBloc>()
                       .add(TimerStarted(duration: timerState.duration)),
-                  icon: Icon(Icons.play_circle_fill_rounded),
                 ),
               ],
               if (timerState is TimerRunInProgress) ...[
-                IconButton(
-                    iconSize: 65,
-                    onPressed: () =>
-                        context.read<TimerBloc>().add(TimerPaused()),
-                    icon: Icon(Icons.pause_circle_outline_rounded)),
-                IconButton(
-                    iconSize: 65,
-                    onPressed: () =>
-                        context.read<TimerBloc>().add(TimerReset()),
-                    icon: Icon(Icons.replay_circle_filled_rounded)),
+                MaterialButton(
+                  color: Colors.redAccent,
+                  elevation: 0,
+                  minWidth: 40,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      width: 60,
+                      child: Text('PAUSE')),
+                  onPressed: () => context.read<TimerBloc>().add(TimerPaused()),
+                ),
+                MaterialButton(
+                  color: Colors.grey[900],
+                  elevation: 0,
+                  minWidth: 40,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      width: 60,
+                      child: Text('RESET')),
+                  onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+                ),
               ],
               if (timerState is TimerRunPause) ...[
-                IconButton(
-                  iconSize: 65,
+                MaterialButton(
+                  color: Colors.blueAccent.shade700,
+                  elevation: 0,
+                  minWidth: 40,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      width: 60,
+                      child: Text('RESUME')),
                   onPressed: () =>
                       context.read<TimerBloc>().add(TimerResumed()),
-                  icon: Icon(Icons.play_circle_outline_rounded),
                 ),
-                IconButton(
-                    iconSize: 65,
-                    onPressed: () =>
-                        context.read<TimerBloc>().add(TimerReset()),
-                    icon: Icon(Icons.replay_circle_filled_rounded)),
-              ],
-              if (timerState is TimerRunComplete) ...[
-                IconButton(
-                    iconSize: 65,
-                    onPressed: () =>
-                        context.read<TimerBloc>().add(TimerReset()),
-                    icon: Icon(Icons.replay_circle_filled_rounded)),
+                MaterialButton(
+                  color: Colors.grey[900],
+                  elevation: 0,
+                  minWidth: 40,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      width: 60,
+                      child: Text('RESET')),
+                  onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+                ),
               ],
               // if (timerState is TimerRunComplete) ...[
               //   IconButton(
